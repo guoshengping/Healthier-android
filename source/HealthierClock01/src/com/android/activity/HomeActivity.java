bf57bd1e5467316365130020c25135956c866456
guosp2012 <guosp2012@163.com>
2012-08-05 20:57:01 +0800

    初始化项目

---
+++ source/HealthierClock01/src/com/android/activity/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.android.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+}
